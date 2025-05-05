4aa00a11976ab57715d1041f8a07b56e02007223
lassegrone <Lasse.groene@icloud.com>
2025-05-05 19:14:19 +0200

    changed sound

--- src/main/java/commands/rechtecommand.java
+++ src/main/java/commands/rechtecommand.java
@@ -1,6 +1,7 @@
 package commands;
 
 import gui.rechtegui;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -23,6 +24,7 @@ public class rechtecommand implements CommandExecutor {
                 return true;
             }
             Player player = (Player) sender;
+            player.playSound(player.getLocation(), Sound.BLOCK_TRIPWIRE_CLICK_ON, 1.0f, 1.0f);
             new rechtegui(plugin).openInventory(player);
         }
         return true;
